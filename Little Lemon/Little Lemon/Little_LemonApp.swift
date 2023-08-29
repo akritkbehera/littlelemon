e11bf47ea1341dfb2356abe96ae1496937698198
akritkbehera <84931167+akritkbehera@users.noreply.github.com>
2023-08-29 10:33:23 +0530

    Done

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by User 2 on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
